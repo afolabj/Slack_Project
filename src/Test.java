3b22f025011c5533bb280206ef20f94280d43ffe
BizimanaValesi <91175062+BizimanaValesi@users.noreply.github.com>
2022-09-26 13:41:21 -0400

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test{
     public static void main(String[] args){
         System.out.println("This is just a test for our project");
+        System.out.println("push to git");
     }
 }
